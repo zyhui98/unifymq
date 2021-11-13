ed4684c961a9797ccdbeed68775f7130c2a5618c
jonny <bm8165@HL>
2021-11-13 16:52:08 +0800

    biz：单独依赖

--- unifymq-biz/express-wp/src/main/java/com/github/zyhui98/express/wp/WpMain.java
+++ unifymq-biz/express-wp/src/main/java/com/github/zyhui98/express/wp/WpMain.java
@@ -2,6 +2,7 @@ package com.github.zyhui98.express.wp;
 
 import cn.hutool.json.JSONUtil;
 import com.github.zyhui98.express.wp.common.WpConfig;
+import com.sf.wp.sdk.dto.OrderFileQueryDTO;
 import com.sf.wp.sdk.dto.OrderStatusParamsDTO;
 import com.sf.wp.sdk.dto.create.OrderCreateRequestDTO;
 import com.sf.wp.sdk.dto.create.OrderExtraInfo;
@@ -10,6 +11,7 @@ import com.sf.wp.sdk.dto.create.OrderLogisticsBaseInfo;
 import com.sf.wp.sdk.server.impl.WpOrderServiceImpl;
 import com.sf.wp.sdk.vo.CreateOrderResponseVO;
 import com.sf.wp.sdk.vo.QueryStatusResponseVO;
+import com.sf.wp.sdk.vo.WsContractInfoVO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
@@ -30,7 +32,13 @@ import static com.sf.wp.sdk.constant.ConfigMapConst.SERVER_PUBLIC_KEY;
  */
 @Slf4j
 public class WpMain {
-    public static String group = "xn";
+    public static String mOrderNo = "XN".concat(System.currentTimeMillis()+"");
+    public static String company = "";
+
+    public static void init() {
+        company = WpConfig.getStr("api", "company");
+        init(company);
+    }
 
     public static void init(String group) {
         //客户固定入参
@@ -46,23 +54,37 @@ public class WpMain {
     }
 
     public static void main(String[] args) {
-        init(group);
-        final CreateOrderResponseVO createOrderResponseVO = orderCreate();
-        if (!createOrderResponseVO.getData().getSuccessList().isEmpty()){
-            final String merchantOrderNo = createOrderResponseVO.getData().getSuccessList().get(0).getMerchantOrderNo();
-            log.info("merchantOrderNo:{}", merchantOrderNo);
-            queryStatus(merchantOrderNo);
-        }
+        init();
+        //queryStatus("XN1635921691904");
+        getFile("XN1636360040643");
+        //final CreateOrderResponseVO createOrderResponseVO = orderCreate();
+        //if (!createOrderResponseVO.getData().getSuccessList().isEmpty()){
+        //    final String merchantOrderNo = createOrderResponseVO.getData().getSuccessList().get(0).getMerchantOrderNo();
+        //    log.info("merchantOrderNo:{}", merchantOrderNo);
+        //    queryStatus(merchantOrderNo);
+        //    getFile(merchantOrderNo);
+        //}
+        //[main] INFO com.github.zyhui98.express.wp.WpMain - CreateOrderResponseVO:CreateOrderResponseVO{code='0', message='成功', data=OrderCreateResultDTO{successList=[CreateOrderSuccDTO{merchantOrderNo='xn000001', wpOrderNo='WP211101000009', failureReason=''}], failureList=[]}}
+        //[main] INFO com.github.zyhui98.express.wp.WpMain - queryStatusResponseVO:{"code":"0","data":[{"mailNo":"","orderNo":"WP211101000009","merchantOrderNo":"xn000001","status":-2,"info":"下单中"}],"message":"成功"}
     }
 
-    public static QueryStatusResponseVO queryStatus(String orderNo) {
+    public static void getFile(String mOrderNo){
+        WpOrderServiceImpl wpOrderService = new WpOrderServiceImpl();
+        OrderFileQueryDTO orderFileQueryDTO = new OrderFileQueryDTO();
+        orderFileQueryDTO.setMerchantOrderNo(mOrderNo);
+        final WsContractInfoVO wsContractInfoVO = wpOrderService.getOrderContract(WpConfig.getStr("api", WpConfig.API_ORDERCONTRACTURL), orderFileQueryDTO);
+        log.info("wsContractInfoVO:{}", JSONUtil.toJsonStr(wsContractInfoVO));
+
+    }
+
+    public static QueryStatusResponseVO queryStatus(String merchantOrderNo) {
         // 实例化WpOrderServiceImpl工具类
         WpOrderServiceImpl wpOrderService = new WpOrderServiceImpl();
         // 构建传参
         OrderStatusParamsDTO queryParamsDTO = new OrderStatusParamsDTO();
-        List<String> orderNoList = new ArrayList<>();
-        orderNoList.add("xn000001");
-        queryParamsDTO.setOrderNos(orderNoList);
+        List<String> merchantOrderNoList = new ArrayList<>();
+        merchantOrderNoList.add(merchantOrderNo);
+        queryParamsDTO.setMerchantOrderNos(merchantOrderNoList);
         // 执行方法
         QueryStatusResponseVO queryStatusResponseVO = wpOrderService.queryStatus(WpConfig.getStr("api", WpConfig.API_ORDERSTATUS), queryParamsDTO);
         log.info("queryStatusResponseVO:{}", JSONUtil.toJsonStr(queryStatusResponseVO));
@@ -70,25 +92,27 @@ public class WpMain {
     }
 
     public static CreateOrderResponseVO orderCreate() {
+
         WpOrderServiceImpl wpOrderService = new WpOrderServiceImpl();
 
         OrderCreateRequestDTO orderCreateRequestDTO = new OrderCreateRequestDTO();
-        orderCreateRequestDTO.setMerchantCode(WpConfig.getStr(group, WpConfig.MY_MERCHANT_CODE));
-        orderCreateRequestDTO.setProductCode(WpConfig.getStr(group, WpConfig.PRODUCTCODE));
-        orderCreateRequestDTO.setAuthName(WpConfig.getStr(group, WpConfig.AUTHNAME));
+        orderCreateRequestDTO.setMerchantCode(WpConfig.getStr(company, WpConfig.MY_MERCHANT_CODE));
+        orderCreateRequestDTO.setProductCode(WpConfig.getStr(company, WpConfig.PRODUCTCODE));
+        orderCreateRequestDTO.setAuthName(WpConfig.getStr(company, WpConfig.AUTHNAME));
 
         OrderInfo orderInfo = new OrderInfo();
         orderCreateRequestDTO.setOrderInfos(Arrays.asList(orderInfo));
         orderInfo.setOrderType(0);
-        orderInfo.setMerchantOrderNo("xn000001");
+        orderInfo.setMerchantOrderNo(mOrderNo);
         orderInfo.setMonthlyCardNo("9999999999");
 
         OrderLogisticsBaseInfo logisticsBaseInfo = new OrderLogisticsBaseInfo();
         orderInfo.setLogisticsBaseInfo(logisticsBaseInfo);
-        logisticsBaseInfo.setdContact("张三");
+        logisticsBaseInfo.setdContact("朱云辉");
         logisticsBaseInfo.setdTel("13524080425");
         logisticsBaseInfo.setdAddress("广东省深圳市福田区华强北桑达工业区404栋首层");
         logisticsBaseInfo.setjContact("朱云辉");
+        logisticsBaseInfo.setjCompany("小牛");
         logisticsBaseInfo.setjTel("13524080425");
         logisticsBaseInfo.setjAddress("上海市闵行区园秀路秀文路莘庄中心3楼");
 
@@ -102,23 +126,21 @@ public class WpMain {
         logisticsBaseInfo.setPayMethod("0");
         logisticsBaseInfo.setExpressType("2");
 
-        orderInfo.setExtraInfos(createOrderExtraInfo("商户订单号", "xn000001",
-                "签署人类型", "个人",
-                "签名区", "接受人签名",
-                "租期", "12个月",
-                "发货时间", "2021-10-31",
-                "设备串码 IMEI", "abcdefg",
-                "型号配置", "iphone 13 pro",
-                "套餐", "标准套餐",
-                "数量", "1",
-                "设备押金或价值（元）", "3000",
-                "月结卡号", "9999999999"
+        orderInfo.setExtraInfos(createOrderExtraInfo("merchantOrderNo", mOrderNo,
+                "partyType", "ALL_NM",
+                "signPos1", "x: 100, y: 100,page:1,signDateBeanType:0）",
+                "stringExt1", "12个月",
+                "stringExt2", "标准套餐",
+                "stringExt3", "abcdefg",
+                "stringExt4", "iphone 13 pro",
+                "stringExt5", "2021-10-31",
+                "stringExt6", "1",
+                "stringExt7", "3000"
         ));
         log.info("orderCreateRequestDTO:{}", JSONUtil.toJsonStr(orderCreateRequestDTO));
         final CreateOrderResponseVO createOrderResponseVO = wpOrderService.createOrder(WpConfig.getStr("api", WpConfig.API_ORDERCREATE),
                 orderCreateRequestDTO);
-        log.info("CreateOrderResponseVO:{}",
-                createOrderResponseVO);
+        log.info("CreateOrderResponseVO:{}",createOrderResponseVO);
 
         return  createOrderResponseVO;
 
--- unifymq-biz/express-wp/src/main/java/com/github/zyhui98/express/wp/common/WpConfig.java
+++ unifymq-biz/express-wp/src/main/java/com/github/zyhui98/express/wp/common/WpConfig.java
@@ -1,6 +1,10 @@
 package com.github.zyhui98.express.wp.common;
 
+import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.setting.Setting;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author jonny
@@ -12,23 +16,33 @@ public class WpConfig {
     public static final String API_ORDERSTATUS = "orderStatus";
     public static final String API_ORDERCONTRACTURL= "orderContractUrl";
     public static final String API_ORDERCREATE= "orderCreate";
-    public static final String WP_RSA_PUBLICKEY = "wp.rsa.publicKey";
-    public static final String MY_RSA_PUBLICKEY = "my.rsa.publicKey";
-    public static final String MY_RSA_PRIVATEKEY = "my.rsa.privateKey";
-    public static final String MY_MERCHANT_CODE = "my.merchant.code";
+    public static final String WP_RSA_PUBLICKEY = "wpPublicKey";
+    public static final String MY_RSA_PUBLICKEY = "publicKey";
+    public static final String MY_RSA_PRIVATEKEY = "privateKey";
+    public static final String MY_MERCHANT_CODE = "merchantCode";
+    public static final String WP_SETTING = "wp.setting";
     public static volatile Setting setting;
-    public static final WpConfig INSTANT = new WpConfig();
+    public static volatile Map<String, Map<String,Object>> settingMap = new ConcurrentHashMap<>(16);
 
     private WpConfig(){
-        setting = new Setting("wp.setting", true);
+        if(ResourceUtil.getStreamSafe(WP_SETTING) != null){
+            setting = new Setting(WP_SETTING, true);
+        }
     }
 
 
     public static String getStr(String group,String key){
-        return setting.getMap(group).get(key);
+        return getStr(group,key, null);
     }
 
     public static String getStr(String group,String key, String value){
+        if(!settingMap.isEmpty()){
+             if(settingMap.containsKey(group)){
+                 return String.valueOf(settingMap.get(group).getOrDefault(key,value));
+             } else  {
+                 return null;
+            }
+        }
         return setting.getMap(group).getOrDefault(key, value);
     }
 
